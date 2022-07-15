8cae2ec1a0c6e7edba791f32c9db2bf062facbef
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2022-07-15 18:57:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+p1 = document.getElementById("p1").value ;
+p2 = document.getElementById("p2").value ;
+function addUser(){
+
+
+localStorage.setItem("player1", p1);
+localStorage.setItem("player1", p2);
+
+
+window.location= "quizgamepage.html";
+
+}
